1d6c8e81ff09a1a3dea4a2aef757bcd411e1adee
dhaval08 <shahdhaval1893@gmail.com>
2016-11-05 18:53:24 -0700

    Updated the UserManger.py file

--- UserManager.py
+++ UserManager.py
@@ -1,25 +1,29 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import csv
+import csv, sys
 
 def invokeUserManager():
     processUserData()
-    print 'Operation successful.' 
+    print 'Operation successful.'
 
 
 def processUserData():
+
+    charSet = set()
+
     global questionTags
     global questionID
     global featureMatrix
     lineNumber = 1
     featureMatrix = np.zeros(shape=(0, 35023))
+    idToFeatureMap = {}
     with open("bytecup2016data/user_info.txt") as tsv:
         for line in csv.reader(tsv, dialect="excel-tab"):
             print lineNumber
             lineNumber += 1
             featureVector = np.zeros(shape=(1, 0))
-            wordIDVector = np.zeros(shape=(1, 31100))
-            charIDVector = np.zeros(shape=(1, 3779))
+            wordIDVector = np.zeros(shape=(1, 37810))
+            charIDVector = np.zeros(shape=(1, 4022))
             tagIDVector = np.zeros(shape=(1, 143))
 
             userID = line[0]
@@ -32,25 +36,29 @@ def processUserData():
 
             wordID = line[2].strip()
             if wordID == '/':
-                wordID = '31099' + '/' + '31099'
+                wordID = '37809' + '/' + '37809'
             wordID = wordID.split('/')
             wordID = map(int, wordID)
 
             charID = line[3].strip()
             if charID == '/':
-                charID = '3778' + '/' + '3778'
+                charID = '4021' + '/' + '4021'
             charID = charID.split('/')
             charID = map(int, charID)
+
+            for cid in wordID:
+                if cid not in charSet:
+                    charSet.add(cid)
 
             tagIDVector[0, tagID] = 1
             wordIDVector[0, wordID] = 1
             charIDVector[0, charID] = 1
 
-            featureVector = np.concatenate((featureVector, np.reshape(userID, (1, 1))), axis=1)
             featureVector = np.concatenate((featureVector, tagIDVector), axis=1)
             featureVector = np.concatenate((featureVector, wordIDVector), axis=1)
             featureVector = np.concatenate((featureVector, charIDVector), axis=1)
-            featureMatrix = np.concatenate((featureMatrix, featureVector), axis=0)
+
+            idToFeatureMap[userID] = featureVector
 
     np.savetxt('bytecup2016data/processedUserData.csv', featureMatrix, delimiter=",")
 
