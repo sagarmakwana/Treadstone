e4e3fc26aa2223b61d6bd9c1c85c3ac4204b2090
Bhavya Sanghavi <Bhavya@Bhavyas-MacBook-Pro.local>
2016-11-06 16:43:54 -0800

    Added script to prepare validation data set

---
+++ ValidateDataMergerManager.py
@@ -0,0 +1,34 @@
+import numpy as np
+import csv
+
+def invokeDataMergeManager():
+    global mergeFile
+    with open('bytecup2016data/validateDataSet.csv', 'wb') as mergeFile:
+        mergeData()
+    print 'Operation successful.'
+
+def mergeData():
+    questionFeatures = np.loadtxt("bytecup2016data/processedQuestionInfo.csv", dtype=str, delimiter=",")
+    userFeatures = np.loadtxt("bytecup2016data/processedUserInfo.csv", dtype=str, delimiter=",")
+    questionIdToFeatureMap = convertToDictionary(questionFeatures)
+    userIdToFeatureMap = convertToDictionary(userFeatures)
+    with open("bytecup2016data/validate_nolabel.txt") as tsv:
+        for line in csv.reader(tsv, dialect=","):
+            questionId = line[0]
+            userId = line[1]
+            combinedFeatureVectorString = questionIdToFeatureMap[questionId] + "," + userIdToFeatureMap[userId] + "\n"
+            mergeFile.write(combinedFeatureVectorString)
+
+
+def convertToDictionary(featureVector):
+    featureMap = {}
+    for keyIndex in range(featureVector.shape[0]):
+        featureValue = ""
+        featureIndex = 0
+        for featureIndex in range(1, featureVector.shape[1] - 1):
+            featureValue += featureVector[keyIndex, featureIndex] + ","
+        featureValue += featureVector[keyIndex, featureIndex + 1]
+        featureMap[featureVector[keyIndex, 0]] = featureValue
+    return featureMap
+
+invokeDataMergeManager()
